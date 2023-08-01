3a6f67b53a57ce4e89810020a9efa406e6eee9f7
YANISUPA <140913814+YANISUPA@users.noreply.github.com>
2023-08-01 21:31:04 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  var skills = ['HTML', 'CSS', 'JS', 'React', 'Node', 'Python', 'C++'];
+  var member = {
+    name: 'John',
+    age: 23,
+    skills: skills
+  };
+  // Code here
+  return member.skills.sort();
+}
